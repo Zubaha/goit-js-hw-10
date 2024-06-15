5182749280a3c2ab9ea03ebc91f87f5f201e0d2b
Zubaha <zubaha4052@gmail.com>
2024-06-15 20:49:54 +0200

    'update'

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,40 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const inputDelay = document.querySelector('.label-delay-input');
+
+function createPromise(delay, status) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (status === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const delay = inputDelay.value;
+  const state = document.querySelector('input[name="state"]:checked').value;
+
+  createPromise(delay, state)
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
